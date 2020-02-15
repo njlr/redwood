61ce0dd4ffe61bd2076bd81c928246a32a34dbc4
njlr <njlr@users.noreply.github.com>
2020-02-15 01:35:25 +0000

    * More stuff

---
+++ plugin/Library.fs
@@ -0,0 +1,14 @@
+﻿namespace HotReload
+
+[<Struct>]
+type Vector2F =
+  {
+    X : float
+    Y : float
+  }
+
+type IPlugin =
+  interface
+    abstract Name : string
+    abstract Update : float -> Vector2F -> Vector2F
+  end
---
+++ sample-plugin/Library.fs
@@ -0,0 +1,10 @@
+﻿namespace HotReload.SamplePlugin
+
+open HotReload
+
+type SamplePlugin () =
+  interface IPlugin with
+    member this.Name
+      with get () = "Sample3"
+    member this.Update dt state =
+      state
